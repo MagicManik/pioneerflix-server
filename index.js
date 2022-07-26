5e57c7d9a2f6374c9bf66875455e5686fe9a696e
MagicManik <manikislam1952@gmail.com>
2022-07-27 02:03:48 +0600

    dotenv and gitignore done

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ async function run() {
         await client.connect();
         const englishMovieCollections = client.db("pioneer_flix").collection("english_movies");
         const banglaMovieCollections = client.db("pioneer_flix").collection("bangla_movies");
-        const paymentCollection = client.db("pioneer_flix").collection("payments");
 
 
 
