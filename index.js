a50868df2ed33decedaf92bd3f9ea5a2faa7da41
MagicManik <manikislam1952@gmail.com>
2022-07-27 00:39:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// server installation: express, cors, dotenv, mongodb, jsonwebtoken
+
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connection with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2tlvc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const englishMovieCollections = client.db("pioneer_flix").collection("english_movies");
+        const banglaMovieCollections = client.db("pioneer_flix").collection("bangla_movies");
+        const paymentCollection = client.db("pioneer_flix").collection("payments");
+
+
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
