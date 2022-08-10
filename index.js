df6c3c7f9ecd4a854c57546d63a74df6f6884b26
MagicManik <manikislam1952@gmail.com>
2022-08-10 18:44:05 +0600

    like api almost done

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ async function run() {
         const paymentCollection = client.db("pioneer_flix").collection("payments");
 
 
-        // videos APIs
+        // videos APIs / Manik Islam Mahi
+        // to read or get videos
         app.get('/videos', async (req, res) => {
             const query = {};
             const cursor = videoCollection.find(query);
@@ -33,6 +34,7 @@ async function run() {
             res.send(videos);
         });
 
+        // Manik Islam Mahi
         app.get('/video/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -41,13 +43,15 @@ async function run() {
         });
 
 
-        // likes APIs
+        // likes APIs / Manik Islam Mahi
+        // to create or post like
         app.post('/like', async (req, res) => {
             const like = req.body;
             const result = await likeCollection.insertOne(like);
             res.send(result);
         })
 
+        // to read or get likes
         app.get('/likes', async (req, res) => {
             const query = {};
             const cursor = likeCollection.find(query);
@@ -55,14 +59,24 @@ async function run() {
             res.send(likes);
         });
 
+        // to delete or remove likes
+        app.delete('/likes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await likeCollection.deleteOne(query);
+            res.send(result);
+        })
 
-        // comments APIs
+
+        // comments APIs / Manik Islam Mahi
+        // to create or post comment
         app.post('/comment', async (req, res) => {
             const item = req.body;
             const result = await commentCollection.insertOne(item);
             res.send(result);
         });
 
+        // to read or get comments
         app.get('/comments', async (req, res) => {
             const query = {};
             const cursor = commentCollection.find(query);
